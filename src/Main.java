2737db50d58155aeacb41458ecda715270c57ccf
Jakub <j.klimiuk95@gmail.com>
2021-09-26 13:09:49 +0200

    Ćw3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +17,44 @@ public class Main {
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        String imie;
+        String nazwisko;
+        int wiek;
+        int nrIndex;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imie:");
+        imie = scan.next();
+        System.out.println("Podaj swoje nazwisko:");
+        nazwisko = scan.next();
+        System.out.println("Podaj wiek:");
+        wiek = scan.nextInt();
+        System.out.println("Podaj nr index:");
+        nrIndex = scan.nextInt();
+        System.out.println("imię: " + imie + ", nazwisko: " + nazwisko + ", wiek: " + wiek + ", nr index: " + nrIndex);
+
+        System.out.printf("imię: " + imie + ", nazwisko: " + nazwisko + ", wiek: " + wiek + ", nr index: " + nrIndex);
+
+
+        Scanner a = new Scanner(System.in);
+        Scanner b = new Scanner(System.in);
+        int pierwsza;
+        int druga;
+        System.out.println("Podaj pierwsza liczbe");
+        pierwsza = a.nextInt();
+        System.out.println("Podaj druga liczbe");
+        druga = b.nextInt();
+        System.out.println("dodawanie: " + (pierwsza + druga));
+        System.out.println("odejmowanie: " + (pierwsza - druga));
+        System.out.println("mnożenie: " + (pierwsza * druga));
+        System.out.println("dzielenie: " + (pierwsza / druga));
+        System.out.println("modulo pierwsza liczba: " + pierwsza%2);
+        System.out.println("modulo druga liczba: " + druga%2);
+
+
+
 
     }
 }
