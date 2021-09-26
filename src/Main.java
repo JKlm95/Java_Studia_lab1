d2cb6821e1d7e59a3462e9d01126b155b0270ebf
Jakub <j.klimiuk95@gmail.com>
2021-09-26 13:26:47 +0200

    Ćw4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,29 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+
+        Scanner pierwszaLiczba = new Scanner(System.in);
+        Scanner drugaliczba = new Scanner(System.in);
+        System.out.println("podaj pierwsza liczbe");
+        int a = pierwszaLiczba.nextInt();
+        System.out.println("podaj druga liczbe");
+        int b = drugaliczba.nextInt();
+
+
+
+        if (a > b){
+            System.out.println(a + " jest większe od " + b);
+        }else if (a == b){
+            System.out.println(a + " jest równe " + b);
+        }
+        else {
+            System.out.println(b + " jest większe od " + a);
+        }
+
+
+
+
+
+
     }
 }
