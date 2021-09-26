1b8a6bb5f14b8c0d07471ea039641cca0f465ec4
Jakub <j.klimiuk95@gmail.com>
2021-09-26 12:26:46 +0200

    Ćw1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Cześć");
+        System.out.printf("Cześć %d",69);
+        System.out.print("Cześć");
 
 
     }
