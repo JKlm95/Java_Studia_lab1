e5c272cb55c2201a6c7851224f4a471e193538d6
Jakub <j.klimiuk95@gmail.com>
2021-09-26 13:48:19 +0200

    Ćw5

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,40 @@ public class Main {
         /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
-
+        /*Scanner w = new Scanner(System.in);
+        System.out.println("Podaj swoj wiek");
+        int wiek = w.nextInt();
+        if (wiek%3 == 0){
+            System.out.println("Podzielny 3");
+        }
+        else System.out.println("Niepodzielny przez 3");
+*/
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
 
+
+      //DO DOKOŃCZENIA
+
+
+
+
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
+
+        Scanner liczba = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+
+        float l = liczba.nextFloat();
+
+        if (l < 10){
+            System.out.println("liczba mniejsza od 10");
+        }else if (l == 10){
+            System.out.println("liczba = 10");
+        }
+        else{
+            System.out.println("liczba wieksza od 10");
+        }
 
     }
 }
